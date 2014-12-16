a0262310d912af6f64130ed244ee800950d777ad
bug2bug <S.Sobko@profitware.ru>
2014-12-16 03:16:55 +0300

    Draw line using Brezenkhem algorithm

--- plot/__init__.py
+++ plot/__init__.py
@@ -3,10 +3,15 @@ __author__ = 'Sergey Sobko'
 import pyb
 from math import sin, pi, cos
 
+from .brezenkhem import draw_line_brezenkhem
+
 STEP = pi * 2 / 128
 LOOP_DELAY_GRAPHICS = 10
 LOOP_DELAY_TEXT = 10
-
+MAXX = 127.0
+MAXY = 31.0
+ACCEL_COEFF_X = MAXX / 90 / 2
+ACCEL_COEFF_Y = MAXY / 90 / 2
 
 class Plot(object):
     def __init__(self):
@@ -41,3 +46,22 @@ class Plot(object):
         return {
             'lcd': self.lcd,
         }
+
+
+class AccelPlot(Plot):
+    def draw_line_for_accel(self, x, y):
+        pixel_x = MAXX / 2 + x * ACCEL_COEFF_X
+        pixel_y = MAXY / 2 - y * ACCEL_COEFF_Y
+        for point in draw_line_brezenkhem((MAXX / 2, MAXY / 2), (pixel_x, pixel_y)):
+            pixel_x, pixel_y = int(point[0]), int(point[1])
+            self.lcd.pixel(pixel_x, pixel_y, 1)
+
+    def loop(self, **kwargs):
+        accel_info = kwargs.get('AccelInfo')
+        if accel_info:
+            x, y, z = map(accel_info.get, ['x', 'y', 'z'])
+            self.lcd.fill(0)
+            self.draw_line_for_accel(x, y)
+            if LOOP_DELAY_GRAPHICS:
+                pyb.delay(LOOP_DELAY_GRAPHICS)
+            self.lcd.show()
--- spiritlevel.py
+++ spiritlevel.py
@@ -2,17 +2,20 @@
 
 import pyb
 
-from plot import Plot
+from plot import Plot, AccelPlot
 from led import LEDIntensity
 from accel import AccelInfo
 
-board_classes = [LEDIntensity, Plot, AccelInfo]
+board_classes = [LEDIntensity, AccelInfo, AccelPlot]
 board_objects = [board_class() for board_class in board_classes]
 
 global_state = dict()
 while True:
     for board_object in board_objects:
-        board_object.loop()
+        if global_state:
+            board_object.loop(**global_state)
+        else:
+            board_object.loop()
         global_state.update({
             board_object.__class__.__name__: board_object.state
         })
